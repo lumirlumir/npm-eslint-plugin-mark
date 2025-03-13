b0ce04e154cd6de298135dd2c83c0b017d6ab2ff
루밀LuMir <rpfos@naver.com>
2025-03-13 14:38:18 +0900

    wip: `no-double-spaces.js`

--- packages/eslint-plugin-mark/src/rules/no-double-spaces/no-double-spaces.js
+++ packages/eslint-plugin-mark/src/rules/no-double-spaces/no-double-spaces.js
@@ -10,15 +10,19 @@
 
 /**
  * @typedef {import("eslint").Rule.RuleModule} RuleModule
+ * @typedef {import("mdast").Text} Text
  */
 
 // --------------------------------------------------------------------------------
 // Helpers
 // --------------------------------------------------------------------------------
 
+const newLineRegex = /\r?\n/;
 const doubleSpacesRegex = /(?<! ) {2}(?! )/g; // Exactly two spaces. no more, no less.
 const leadingSpacesRegex = /^ */;
 const trailingSpacesRegex = / *$/;
+const singleSpace = ' ';
+const indexOffset = 1; // `index` is 0-based, but `column` is 1-based.
 
 // --------------------------------------------------------------------------------
 // Rule Definition
@@ -28,13 +32,16 @@ const trailingSpacesRegex = / *$/;
 export default {
   meta: {
     type: 'problem',
+
     docs: {
       recommended: true,
       description:
         'Disallow double spaces in text, except for leading and trailing spaces',
       url: 'https://github.com/lumirlumir/npm-eslint-plugin-mark',
     },
+
     fixable: 'whitespace',
+
     messages: {
       noDoubleSpaces:
         'Double spaces are not allowed except for leading and trailing spaces.',
@@ -43,25 +50,70 @@ export default {
 
   create(context) {
     return {
+      /** @param {Text} node */
       text(node) {
-        const trimmedValue = node.value.trim();
+        /**
+         * - Supports both LF and CRLF line endings.
+         * - In JavaScript, `"\n"` represents a newline character and splits text accordingly.
+         *   However, in Markdown, writing `abcd\ndefg` treats `\n` as plain text (backslash + 'n'), not a newline.
+         *   Only actual line breaks in Markdown (`abcd↵defg`) are stored as `"\n"` and split properly.
+         */
+        const lines = node.value.split(newLineRegex);
 
-        if (trimmedValue.match(doubleSpacesRegex)) {
-          context.report({
-            loc: node.position,
-            messageId: 'noDoubleSpaces',
-            fix(fixer) {
-              const leadingSpaces = node.value.match(leadingSpacesRegex)[0];
-              const trailingSpaces = node.value.match(trailingSpacesRegex)[0];
-              const fixedDoubleSpaces = trimmedValue.replaceAll(doubleSpacesRegex, ' ');
+        lines.forEach((line, lineNumber) => {
+          const matches = [...line.trim().matchAll(doubleSpacesRegex)]; // Remove leading and trailing spaces
 
-              return fixer.replaceText(
-                node,
-                `${leadingSpaces}${fixedDoubleSpaces}${trailingSpaces}`,
-              );
-            },
-          });
-        }
+          if (matches.length > 0) {
+            matches.forEach(match => {
+              const doubleSpaceslength = match[0].length;
+              const leadingSpacesLength = line.match(leadingSpacesRegex)[0].length;
+
+              const matchIndexStart = leadingSpacesLength + match.index; // `match.index` does not include leading spaces
+              const matchIndexEnd = matchIndexStart + doubleSpaceslength;
+
+              /**
+               * Current node's start line + relative line number
+               */
+              const locLine = node.position.start.line + lineNumber;
+              /**
+               * Consider the starting column position for the first line, otherwise calculate from the start of the line
+               * @param {number} matchIndex
+               */
+              const locColumn = matchIndex =>
+                lineNumber === 0
+                  ? node.position.start.column + matchIndex
+                  : indexOffset + matchIndex;
+
+              context.report({
+                loc: {
+                  start: {
+                    line: locLine,
+                    column: locColumn(matchIndexStart),
+                  },
+                  end: {
+                    line: locLine,
+                    column: locColumn(matchIndexEnd),
+                  },
+                },
+
+                messageId: 'noDoubleSpaces',
+
+                fix(fixer) {
+                  const leadingSpaces = node.value.match(leadingSpacesRegex)[0];
+                  const trailingSpaces = node.value.match(trailingSpacesRegex)[0];
+                  const fixedDoubleSpaces = node.value
+                    .trim()
+                    .replaceAll(doubleSpacesRegex, singleSpace);
+
+                  return fixer.replaceText(
+                    node,
+                    `${leadingSpaces}${fixedDoubleSpaces}${trailingSpaces}`,
+                  );
+                },
+              });
+            });
+          }
+        });
       },
     };
   },
