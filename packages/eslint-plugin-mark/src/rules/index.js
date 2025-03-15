60fe54dc5ff1affd965ce35a65a3efb3a2f4098b
루밀LuMir <rpfos@naver.com>
2025-03-15 16:10:54 +0900

    wip

--- packages/eslint-plugin-mark/src/rules/index.js
+++ packages/eslint-plugin-mark/src/rules/index.js
@@ -1,7 +1,8 @@
 import noCurlyQuotes from './no-curly-quotes/index.js';
 import noDoubleSpaces from './no-double-spaces/index.js';
 
-export default [noCurlyQuotes, noDoubleSpaces].map(rule => ({
+export default [noCurlyQuotes, noDoubleSpaces].reduce((rulesObject, rule) => {
   // @ts-ignore -- TODO: https://github.com/eslint/eslint/issues/19521, https://github.com/eslint/eslint/issues/19523
-  [rule.meta.docs.name]: rule,
-}));
+  rulesObject[rule.meta.docs.name] = rule;
+  return rulesObject;
+}, {});
