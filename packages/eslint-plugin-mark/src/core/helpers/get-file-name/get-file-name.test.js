570a3f130fc8473d399cd5993292941a483cd206
루밀LuMir <rpfos@naver.com>
2025-03-15 15:31:35 +0900

    wip: `get-file-name.js`

---
+++ packages/eslint-plugin-mark/src/core/helpers/get-file-name/get-file-name.test.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Test for `get-file-name.js`
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { describe, it } from 'node:test';
+import { strictEqual } from 'node:assert';
+
+import getFileName from './get-file-name.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe(getFileName(import.meta.url), () => {
+  it('should return the file name of the module', () => {
+    strictEqual(getFileName(import.meta.url), 'get-file-name.test');
+  });
+});
---
+++ packages/eslint-plugin-mark/src/core/helpers/get-file-name/index.js
@@ -0,0 +1,3 @@
+import getFileName from './get-file-name.js';
+
+export default getFileName;
