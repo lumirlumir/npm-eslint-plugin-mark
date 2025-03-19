72287f5303dfcc704b3acba28afa70fa01c7ce85
루밀LuMir <rpfos@naver.com>
2025-03-19 22:27:27 +0900

    wip

---
+++ packages/eslint-plugin-mark/src/rules/code-lang-shorthand/code-lang-shorthand.js
@@ -0,0 +1,177 @@
+/**
+ * @fileoverview
+ * @author 루밀LuMir(lumirlumir)
+ * @see https://shiki.style/languages#bundled-languages
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { getFileName } from '../../core/helpers/index.js';
+
+// --------------------------------------------------------------------------------
+// Typedefs
+// --------------------------------------------------------------------------------
+
+/**
+ * @typedef {import("@eslint/markdown").RuleModule} RuleModule
+ * @typedef {import("mdast").Code} Code
+ */
+
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
+/** @type {Record<string, string>} */
+const langShorthandMap = Object.freeze({
+  asciidoc: 'adoc',
+  batch: 'bat',
+  berry: 'be',
+  bsl: '1c',
+  cadence: 'cdc',
+  clojure: 'clj',
+  codeql: 'ql',
+  coffeescript: 'coffee',
+  'common-lisp': 'lisp',
+  csharp: 'c#',
+  cypher: 'cql',
+  dockerfile: 'docker',
+  'emacs-lisp': 'elisp',
+  erlang: 'erl',
+  fluent: 'ftl',
+  'fortran-fixed-form': 'f',
+  fsharp: 'f#',
+  'glimmer-js': 'gjs',
+  'glimmer-ts': 'gts',
+  graphql: 'gql',
+  handlebars: 'hbs',
+  haskell: 'hs',
+  properties: 'ini',
+  javascript: 'js',
+  jssm: 'fsl',
+  julia: 'jl',
+  kotlin: 'kt',
+  kusto: 'kql',
+  lean4: 'lean',
+  makefile: 'make',
+  markdown: 'md',
+  mermaid: 'mmd',
+  mipsasm: 'mips',
+  narrat: 'nar',
+  nextflow: 'nf',
+  nushell: 'nu',
+  'objective-c': 'objc',
+  pot: 'po',
+  potx: 'po',
+  powershell: 'ps',
+  protobuf: 'proto',
+  jade: 'pug',
+  python: 'py',
+  perl6: 'raku',
+  regexp: 'regex',
+  ruby: 'rb',
+  rust: 'rs',
+  '1c-query': 'sdbl',
+  shaderlab: 'shader',
+  shellscript: 'sh',
+  shell: 'sh',
+  bash: 'sh',
+  zsh: 'sh',
+  shellsession: 'console',
+  'closure-templates': 'soy',
+  splunk: 'spl',
+  stylus: 'styl',
+  talonscript: 'talon',
+  terraform: 'tf',
+  text: 'txt',
+  typescript: 'ts',
+  typespec: 'tsp',
+  typst: 'typ',
+  vimscript: 'vim',
+  vyper: 'vy',
+  wikitext: 'wiki',
+  mediawiki: 'wiki',
+  wolfram: 'wl',
+  yaml: 'yml',
+});
+
+// --------------------------------------------------------------------------------
+// Rule Definition
+// --------------------------------------------------------------------------------
+
+/** @type {RuleModule} */
+export default {
+  meta: {
+    type: 'problem',
+
+    docs: {
+      // @ts-ignore -- TODO: https://github.com/eslint/eslint/issues/19521, https://github.com/eslint/eslint/issues/19523
+      name: getFileName(import.meta.url),
+      recommended: true,
+      description: '',
+      url: 'https://github.com/lumirlumir/npm-eslint-plugin-mark',
+    },
+
+    fixable: 'code',
+
+    messages: {
+      codeLangShorthand: '`{{ lang }}` should be shortened to {{ langShorthand }}.',
+    },
+
+    language: 'markdown',
+
+    dialects: ['commonmark', 'gfm'],
+  },
+
+  create(context) {
+    return {
+      /** @param {Code} node */
+      code(node) {
+        const langShorthand = langShorthandMap[node.lang];
+
+        if (langShorthand === undefined) return;
+
+        // @ts-ignore -- TODO: https://github.com/eslint/markdown/issues/323
+        const match = context.sourceCode.getText(node).match(node.lang);
+
+        const matchIndexStart = match.index;
+        const matchIndexEnd = matchIndexStart + match[0].length;
+
+        context.report({
+          loc: {
+            start: {
+              line: node.position.start.line,
+              column: node.position.start.column + matchIndexStart,
+            },
+            end: {
+              line: node.position.start.line,
+              column: node.position.start.column + matchIndexEnd,
+            },
+          },
+
+          data: {
+            lang: node.lang,
+            langShorthand,
+          },
+
+          messageId: 'codeLangShorthand',
+
+          fix(fixer) {
+            return fixer.replaceTextRange(
+              [
+                node.position.start.offset + matchIndexStart,
+                node.position.start.offset + matchIndexEnd,
+              ],
+              langShorthand,
+            );
+          },
+        });
+      },
+    };
+  },
+};
+
+// Options:
+// - ignores: ['js', 'ts']
+// - override: { 'c++': 'cpp' }
