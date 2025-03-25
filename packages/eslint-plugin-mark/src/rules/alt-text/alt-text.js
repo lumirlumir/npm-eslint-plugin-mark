459df9ce4c7e263b4c1d113a1488ab4466c56eb0
루밀LuMir <rpfos@naver.com>
2025-03-25 20:32:25 +0900

    wip

---
+++ packages/eslint-plugin-mark/src/rules/alt-text/alt-text.js
@@ -0,0 +1,96 @@
+/**
+ * @fileoverview Rule to enforce the use of alternative text for images.
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import * as cheerio from 'cheerio';
+import { getFileName, getRuleDocsUrl } from '../../core/helpers/index.js';
+
+// --------------------------------------------------------------------------------
+// Typedefs
+// --------------------------------------------------------------------------------
+
+/**
+ * @typedef {import("@eslint/markdown").RuleModule} RuleModule
+ * @typedef {import("mdast").Image} Image
+ * @typedef {import("mdast").ImageReference} ImageReference
+ * @typedef {import("mdast").Html} Html
+ */
+
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
+const ruleName = getFileName(import.meta.url);
+
+// --------------------------------------------------------------------------------
+// Rule Definition
+// --------------------------------------------------------------------------------
+
+/** @type {RuleModule} */
+export default {
+  meta: {
+    type: 'problem',
+
+    docs: {
+      // @ts-ignore -- TODO: https://github.com/eslint/eslint/issues/19521, https://github.com/eslint/eslint/issues/19523
+      name: ruleName,
+      recommended: true,
+      description: 'Enforce the use of alternative text for images.',
+      url: getRuleDocsUrl(ruleName),
+    },
+
+    messages: {
+      altText: 'Images should have an alternative text (alt text).',
+    },
+
+    language: 'markdown',
+
+    dialects: ['commonmark', 'gfm'],
+  },
+
+  create(context) {
+    return {
+      /** @param {Image} node */
+      image(node) {
+        if (node.alt === '') {
+          context.report({
+            // @ts-ignore -- TODO
+            node,
+            messageId: 'altText',
+          });
+        }
+      },
+
+      /** @param {ImageReference} node */
+      imageReference(node) {
+        if (node.alt === '') {
+          context.report({
+            // @ts-ignore -- TODO
+            node,
+            messageId: 'altText',
+          });
+        }
+      },
+
+      /** @param {Html} node */
+      html(node) {
+        const $ = cheerio.load(node.value);
+
+        $('img').each((_, elem) => {
+          if (!elem.attribs.alt) {
+            context.report({
+              // @ts-ignore -- TODO
+              node,
+              messageId: 'altText',
+            });
+          }
+        });
+      },
+    };
+  },
+};
---
+++ packages/eslint-plugin-mark/src/rules/alt-text/alt-text.test.js
@@ -0,0 +1,151 @@
+/**
+ * @fileoverview Test for `alt-text.js`.
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { test } from 'node:test';
+
+import { ruleTesterCommonmark, ruleTesterGfm } from '../../core/rule-tester/index.js';
+import rule from './alt-text.js';
+
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
+const { name } = rule.meta.docs;
+const altText = 'altText';
+
+// --------------------------------------------------------------------------------
+// Testcases
+// --------------------------------------------------------------------------------
+
+const tests = {
+  valid: [
+    {
+      name: 'Empty',
+      code: '',
+    },
+    {
+      name: 'Image node with alt text',
+      code: '![alt text](https://example.com/image.jpg)',
+    },
+    {
+      name: 'ImageReference node with alt text',
+      code: `
+![alt text][image]
+
+[image]: https://example.com/image.jpg
+`,
+    },
+    {
+      name: 'Html node with alt text',
+      code: '<img src="https://example.com/image.jpg" alt="alt text">',
+    },
+    {
+      name: 'Nested Html node with alt text',
+      code: `
+<div>
+  <img src="https://example.com/image.jpg" alt="alt text">
+</div>
+`,
+    },
+  ],
+
+  invalid: [
+    {
+      name: 'Image node without alt text',
+      code: '![](https://example.com/image.jpg)',
+      errors: [
+        {
+          messageId: altText,
+          line: 1,
+          column: 1,
+          endLine: 1,
+          endColumn: 35,
+        },
+      ],
+    },
+    {
+      name: 'ImageReference node without alt text',
+      code: `
+![][image]
+
+[image]: https://example.com/image.jpg
+`,
+      errors: [
+        {
+          messageId: altText,
+          line: 2,
+          column: 1,
+          endLine: 2,
+          endColumn: 11,
+        },
+      ],
+    },
+    {
+      name: 'Html node without alt text',
+      code: '<img src="https://example.com/image.jpg">',
+      errors: [
+        {
+          messageId: altText,
+          line: 1,
+          column: 1,
+          endLine: 1,
+          endColumn: 42,
+        },
+      ],
+    },
+    {
+      name: 'Html node with empty alt text',
+      code: '<img src="https://example.com/image.jpg" alt="">',
+      errors: [
+        {
+          messageId: altText,
+          line: 1,
+          column: 1,
+          endLine: 1,
+          endColumn: 49,
+        },
+      ],
+    },
+    {
+      name: 'Nested Html node without alt text',
+      code: `
+<div>
+  <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg"><br>
+  <a href="https://www.google.com">google</a><br>
+  <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg"><br>
+</div>
+`,
+      errors: [
+        {
+          messageId: altText,
+          line: 2,
+          column: 1,
+          endLine: 6,
+          endColumn: 7,
+        },
+        {
+          messageId: altText,
+          line: 2,
+          column: 1,
+          endLine: 6,
+          endColumn: 7,
+        },
+      ],
+    },
+  ],
+};
+
+// --------------------------------------------------------------------------------
+// Test Runner
+// --------------------------------------------------------------------------------
+
+test(name, () => {
+  ruleTesterCommonmark.run(name, rule, tests);
+  ruleTesterGfm.run(name, rule, tests);
+});
