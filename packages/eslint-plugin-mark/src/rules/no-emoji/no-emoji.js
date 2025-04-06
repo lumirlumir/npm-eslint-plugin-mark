d0fe95489fb0d5442fe68005a81bae5b3bcbd21f
루밀LuMir <rpfos@naver.com>
2025-04-06 23:42:01 +0900

    wip

--- packages/eslint-plugin-mark/src/rules/no-emoji/no-emoji.js
+++ packages/eslint-plugin-mark/src/rules/no-emoji/no-emoji.js
@@ -18,7 +18,7 @@ import { getRuleDocsUrl } from '../../core/helpers/index.js';
 
 /**
  * @typedef {import("@eslint/markdown").RuleModule} RuleModule
- * @typedef {import("../../core/types").TextExt} TextExt
+ * @typedef {import("../../core/types.d.ts").TextExt} TextExt
  */
 
 // --------------------------------------------------------------------------------
