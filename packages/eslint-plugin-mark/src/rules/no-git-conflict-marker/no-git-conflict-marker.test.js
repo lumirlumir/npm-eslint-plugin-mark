f61190d1ba4955f0c0f1e3d3f1b425fd537994f2
루밀LuMir <rpfos@naver.com>
2025-04-08 15:02:08 +0900

    wip

--- packages/eslint-plugin-mark/src/rules/no-git-conflict-marker/no-git-conflict-marker.test.js
+++ packages/eslint-plugin-mark/src/rules/no-git-conflict-marker/no-git-conflict-marker.test.js
@@ -27,6 +27,7 @@ const noGitConflictMarker = 'noGitConflictMarker';
 
 const tests = {
   valid: [
+    // Basic
     {
       name: 'Empty',
       code: '',
@@ -34,6 +35,18 @@ const tests = {
     {
       name: 'Empty string',
       code: '  ',
+    },
+    {
+      name: '`<` repeats 6 times',
+      code: '<<<<<<',
+    },
+    {
+      name: '`=` repeats 6 times',
+      code: '======',
+    },
+    {
+      name: '`>` repeats 6 times',
+      code: '>>>>>>',
     },
     {
       name: '`<` repeats 8 times',
@@ -46,6 +59,26 @@ const tests = {
     {
       name: '`>` repeats 8 times',
       code: '>>>>>>>>',
+    },
+
+    // Options
+    {
+      name: '`skipCode` option: code block should be skipped (`>`)',
+      code: `\`\`\`md
+>>>>>>> ab18d2f0f5151ab0c927a12eb0a64f8170762eff
+\`\`\``,
+    },
+    {
+      name: '`skipCode` option: code block should be skipped (`=`)',
+      code: `\`\`\`md
+=======
+\`\`\``,
+    },
+    {
+      name: '`skipCode` option: code block should be skipped (`<`)',
+      code: `\`\`\`md
+<<<<<<< HEAD
+\`\`\``,
     },
   ],
 
@@ -87,6 +120,95 @@ const tests = {
           column: 1,
           endLine: 1,
           endColumn: 8,
+        },
+      ],
+    },
+    {
+      name: 'Real world example',
+      code: '<<<<<<< HEAD\nHello\n=======\nWorld\n>>>>>>> ab18d2f0f5151ab0c927a12eb0a64f8170762eff',
+      errors: [
+        {
+          messageId: noGitConflictMarker,
+          line: 1,
+          column: 1,
+          endLine: 1,
+          endColumn: 8,
+        },
+        {
+          messageId: noGitConflictMarker,
+          line: 3,
+          column: 1,
+          endLine: 3,
+          endColumn: 8,
+        },
+        {
+          messageId: noGitConflictMarker,
+          line: 5,
+          column: 1,
+          endLine: 5,
+          endColumn: 8,
+        },
+      ],
+    },
+
+    // Options
+    {
+      name: '`skipCode: false` option: code block should not be skipped (`>`)',
+      code: `\`\`\`md
+>>>>>>> ab18d2f0f5151ab0c927a12eb0a64f8170762eff
+\`\`\``,
+      errors: [
+        {
+          messageId: noGitConflictMarker,
+          line: 2,
+          column: 1,
+          endLine: 2,
+          endColumn: 8,
+        },
+      ],
+      options: [
+        {
+          skipCode: false,
+        },
+      ],
+    },
+    {
+      name: '`skipCode: false` option: code block should not be skipped (`=`)',
+      code: `\`\`\`md
+=======
+\`\`\``,
+      errors: [
+        {
+          messageId: noGitConflictMarker,
+          line: 2,
+          column: 1,
+          endLine: 2,
+          endColumn: 8,
+        },
+      ],
+      options: [
+        {
+          skipCode: false,
+        },
+      ],
+    },
+    {
+      name: '`skipCode: false` option: code block should not be skipped (`<`)',
+      code: `\`\`\`md
+<<<<<<< HEAD
+\`\`\``,
+      errors: [
+        {
+          messageId: noGitConflictMarker,
+          line: 2,
+          column: 1,
+          endLine: 2,
+          endColumn: 8,
+        },
+      ],
+      options: [
+        {
+          skipCode: false,
         },
       ],
     },
