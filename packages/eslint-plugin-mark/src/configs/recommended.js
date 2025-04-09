f327de66f258810b3c9c824f2be12fe2af002ebf
루밀LuMir <rpfos@naver.com>
2025-04-09 22:01:34 +0900

    wip

--- packages/eslint-plugin-mark/src/configs/recommended.js
+++ packages/eslint-plugin-mark/src/configs/recommended.js
@@ -35,6 +35,7 @@ export default function recommended(parserMode) {
       'mark/alt-text': 'error',
       'mark/code-lang-shorthand': 'error',
       'mark/no-bold-paragraph': 'error',
+      'mark/no-control-character': 'error',
       'mark/no-curly-quote': 'error',
       'mark/no-double-space': 'error',
       'mark/no-git-conflict-marker': 'error',
