cfb2cae73b9baaef4793516568f0ab69c28008a9
루밀LuMir <rpfos@naver.com>
2025-04-18 18:40:45 +0900

    wip: update `types.d.ts`

--- packages/eslint-plugin-mark/src/core/types.d.ts
+++ packages/eslint-plugin-mark/src/core/types.d.ts
@@ -3,5 +3,39 @@
  */
 
 // --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import type {
+  MarkdownRuleDefinition,
+  MarkdownRuleDefinitionTypeOptions,
+} from '@eslint/markdown/types';
+
+// --------------------------------------------------------------------------------
 // Typedefs
 // --------------------------------------------------------------------------------
+
+export type RuleModuleTypeOptions = Omit<
+  MarkdownRuleDefinitionTypeOptions,
+  'ExtRuleDocs'
+>;
+
+export type RuleModule<Options extends Partial<RuleModuleTypeOptions> = object> =
+  MarkdownRuleDefinition<
+    Options & {
+      ExtRuleDocs: {
+        /**
+         * Indicates whether this rule is part of the strict configuration.
+         */
+        strict?: boolean | RuleModuleTypeOptions['RuleOptions'];
+        /**
+         * Indicates whether this rule is part of the style configuration.
+         */
+        style?: boolean | RuleModuleTypeOptions['RuleOptions'];
+        /**
+         * Indicates whether this rule is part of the typography configuration.
+         */
+        typography?: boolean | RuleModuleTypeOptions['RuleOptions'];
+      };
+    }
+  >;
