46b3491bfa7f7a1f99767faf806f42311ae88225
루밀LuMir <rpfos@naver.com>
2025-10-19 16:05:34 +0900

    wip

--- packages/eslint-plugin-mark/src/core/ast/skip-ranges/skip-ranges.test.js
+++ packages/eslint-plugin-mark/src/core/ast/skip-ranges/skip-ranges.test.js
@@ -92,57 +92,46 @@ describe(getFileName(import.meta.url), () => {
       });
     });
 
-    /*
-
     describe('Strong', () => {
       it('**hello**', () => {
-        const ignoredPositions = new SkipRanges();
+        const skipRanges = new SkipRanges();
 
-        ignoredPositions.push({
-          start: { line: 1, column: 1, offset: 0 },
-          end: { line: 1, column: 10, offset: 9 },
-        });
+        skipRanges.push([0, 9]);
 
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 1 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 2 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 3 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 4 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 5 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 6 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 7 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 8 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 9 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 10 }), false);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 1 }), false);
+        strictEqual(skipRanges.isInSkipRange(0), true);
+        strictEqual(skipRanges.isInSkipRange(1), true);
+        strictEqual(skipRanges.isInSkipRange(2), true);
+        strictEqual(skipRanges.isInSkipRange(3), true);
+        strictEqual(skipRanges.isInSkipRange(4), true);
+        strictEqual(skipRanges.isInSkipRange(5), true);
+        strictEqual(skipRanges.isInSkipRange(6), true);
+        strictEqual(skipRanges.isInSkipRange(7), true);
+        strictEqual(skipRanges.isInSkipRange(8), true);
+        strictEqual(skipRanges.isInSkipRange(9), false);
+        strictEqual(skipRanges.isInSkipRange(10), false);
       });
     });
 
     describe('Text', () => {
       it('hello\nworld', () => {
-        const ignoredPositions = new SkipRanges();
+        const skipRanges = new SkipRanges();
 
-        ignoredPositions.push({
-          start: { line: 1, column: 1, offset: 0 },
-          end: { line: 2, column: 6, offset: 11 },
-        });
+        skipRanges.push([0, 11]);
 
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 1 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 2 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 3 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 4 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 5 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 1, column: 6 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 1 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 2 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 3 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 4 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 5 }), true);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 2, column: 6 }), false);
-        strictEqual(ignoredPositions.isIgnoredPoint({ line: 3, column: 1 }), false);
+        strictEqual(skipRanges.isInSkipRange(0), true);
+        strictEqual(skipRanges.isInSkipRange(1), true);
+        strictEqual(skipRanges.isInSkipRange(2), true);
+        strictEqual(skipRanges.isInSkipRange(3), true);
+        strictEqual(skipRanges.isInSkipRange(4), true);
+        strictEqual(skipRanges.isInSkipRange(5), true);
+        strictEqual(skipRanges.isInSkipRange(6), true);
+        strictEqual(skipRanges.isInSkipRange(7), true);
+        strictEqual(skipRanges.isInSkipRange(8), true);
+        strictEqual(skipRanges.isInSkipRange(9), true);
+        strictEqual(skipRanges.isInSkipRange(10), true);
+        strictEqual(skipRanges.isInSkipRange(11), false);
+        strictEqual(skipRanges.isInSkipRange(12), false);
       });
     });
-  });
-
-  */
   });
 });
