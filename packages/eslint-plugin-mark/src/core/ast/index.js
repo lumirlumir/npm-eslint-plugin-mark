89330af3d47178d210a23c9307337a37a5aed36d
루밀LuMir <rpfos@naver.com>
2025-10-21 19:07:56 +0900

    wip

--- packages/eslint-plugin-mark/src/core/ast/index.js
+++ packages/eslint-plugin-mark/src/core/ast/index.js
@@ -1,4 +1,4 @@
-import getElementsByTagName from './html/index.js';
+import getElementsByTagName from './html.js';
 import IgnoredPositions from './ignored-positions/index.js';
 import ReferenceDefinitionHandler from './reference-definition-handler/index.js';
 import SkipRanges from './skip-ranges.js';
