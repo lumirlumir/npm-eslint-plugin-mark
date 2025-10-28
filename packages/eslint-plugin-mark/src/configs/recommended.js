13a154dc8588f54b93e98e0b63d5b03fbb451aff
루밀LuMir <rpfos@naver.com>
2025-10-28 23:12:04 +0900

    wip

--- packages/eslint-plugin-mark/src/configs/recommended.js
+++ packages/eslint-plugin-mark/src/configs/recommended.js
@@ -27,10 +27,13 @@ export default {
   name: 'mark/recommended',
   rules: {
     'mark/alt-text': 'error',
+    'mark/code-lang-shorthand': 'error',
     'mark/no-bold-paragraph': 'error',
     'mark/no-control-character': 'error',
+    'mark/no-curly-quote': 'error',
     'mark/no-double-space': 'error',
     'mark/no-git-conflict-marker': 'error',
+    'mark/no-irregular-dash': 'error',
     'mark/no-irregular-whitespace': 'error',
     'mark/no-unused-definition': 'error',
   },
