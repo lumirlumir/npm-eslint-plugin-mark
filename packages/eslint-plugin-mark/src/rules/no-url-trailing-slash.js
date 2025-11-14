7629ccb9ff0f3cbcd21f31551f2ae7b303033134
루밀LuMir <rpfos@naver.com>
2025-11-14 13:34:18 +0900

    feat(eslint-plugin-mark): create `no-url-trailing-slash` rule

--- packages/eslint-plugin-mark/src/rules/no-url-trailing-slash.js
+++ packages/eslint-plugin-mark/src/rules/no-url-trailing-slash.js
@@ -1,1 +1,50 @@
-// TODO
+/**
+ * @fileoverview TODO
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { URL_RULE_DOCS } from '../core/constants.js';
+
+// --------------------------------------------------------------------------------
+// Typedef
+// --------------------------------------------------------------------------------
+
+/**
+ * @import { RuleModule } from '../core/types.js';
+ * @typedef {[]} RuleOptions
+ * @typedef {'noUrlTrailingSlash'} MessageIds
+ */
+
+// --------------------------------------------------------------------------------
+// Rule Definition
+// --------------------------------------------------------------------------------
+
+/** @type {RuleModule<RuleOptions, MessageIds>} */
+export default {
+  meta: {
+    type: 'problem',
+
+    docs: {
+      description: 'Disallow URL trailing slash',
+      url: URL_RULE_DOCS('no-url-trailing-slash'),
+      recommended: false,
+      stylistic: false,
+    },
+
+    messages: {
+      noUrlTrailingSlash: 'URL trailing slash is not allowed.',
+    },
+
+    language: 'markdown',
+
+    dialects: ['commonmark', 'gfm'],
+  },
+
+  create(/* context */) {
+    return {};
+  },
+};
---
+++ packages/eslint-plugin-mark/src/rules/no-url-trailing-slash.test.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Test for `no-url-trailing-slash.js`.
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { getFileName, ruleTester } from '../core/tests/index.js';
+import rule from './no-url-trailing-slash.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+ruleTester(getFileName(import.meta.url), rule, {
+  valid: [
+    {
+      name: 'Empty',
+      code: '',
+    },
+    {
+      name: 'Empty string',
+      code: '  ',
+    },
+  ],
+
+  invalid: [],
+});
