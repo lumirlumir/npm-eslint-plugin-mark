2ae45d6911a358334afcd763868398fefe972fb2
루밀LuMir <rpfos@naver.com>
2025-11-15 23:44:20 +0900

    wip

--- packages/eslint-plugin-mark/src/rules/allow-link-url.test.js
+++ packages/eslint-plugin-mark/src/rules/allow-link-url.test.js
@@ -1,1 +1,21 @@
-// TODO
+/**
+ * @fileoverview Test for `allow-link-url.js`.
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
+import { getFileName, ruleTester } from '../core/tests/index.js';
+import rule from './allow-link-url.js';
+
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+ruleTester(getFileName(import.meta.url), rule, {
+  valid: ['', '  '],
+
+  invalid: [],
+});
