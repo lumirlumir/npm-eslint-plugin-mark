af80418b991d84a170cf84ba1bb9425939c4accc
루밀LuMir <rpfos@naver.com>
2025-11-15 15:25:17 +0900

    wip

--- packages/eslint-plugin-mark/src/rules/no-url-trailing-slash.js
+++ packages/eslint-plugin-mark/src/rules/no-url-trailing-slash.js
@@ -7,6 +7,7 @@
 // Import
 // --------------------------------------------------------------------------------
 
+import { getElementsByTagName } from '../core/ast/index.js';
 import { URL_RULE_DOCS } from '../core/constants.js';
 
 // --------------------------------------------------------------------------------
@@ -44,7 +45,93 @@ export default {
     dialects: ['commonmark', 'gfm'],
   },
 
-  create(/* context */) {
-    return {};
+  create(context) {
+    const { sourceCode } = context;
+
+    return {
+      image(node) {
+        const url = new URL(node.url);
+
+        url.search = '';
+        url.hash = '';
+
+        if (url.href.endsWith('/')) {
+          context.report({
+            node,
+            messageId: 'noUrlTrailingSlash',
+          });
+        }
+      },
+
+      link(node) {
+        const url = new URL(node.url);
+
+        url.search = '';
+        url.hash = '';
+
+        if (url.href.endsWith('/')) {
+          context.report({
+            node,
+            messageId: 'noUrlTrailingSlash',
+          });
+        }
+      },
+
+      html(node) {
+        const html = sourceCode.getText(node);
+
+        getElementsByTagName(html, 'a').forEach(({ attrs /* sourceCodeLocation */ }) => {
+          for (const { name, value } of attrs) {
+            if (name === 'href') {
+              const url = new URL(value);
+
+              url.search = '';
+              url.hash = '';
+
+              if (url.href.endsWith('/')) {
+                context.report({
+                  node,
+                  messageId: 'noUrlTrailingSlash',
+                });
+              }
+            }
+          }
+        });
+
+        getElementsByTagName(html, 'img').forEach(
+          ({ attrs /* sourceCodeLocation */ }) => {
+            for (const { name, value } of attrs) {
+              if (name === 'src') {
+                const url = new URL(value);
+
+                url.search = '';
+                url.hash = '';
+
+                if (url.href.endsWith('/')) {
+                  context.report({
+                    node,
+                    messageId: 'noUrlTrailingSlash',
+                  });
+                }
+              }
+            }
+          },
+        );
+      },
+
+      definition(node) {
+        const url = new URL(node.url);
+
+        url.search = '';
+        url.hash = '';
+
+        if (url.href.endsWith('/')) {
+          context.report({
+            node,
+            messageId: 'noUrlTrailingSlash',
+          });
+        }
+      },
+    };
   },
 };
