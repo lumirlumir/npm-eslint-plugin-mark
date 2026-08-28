17093d7e49a634fbce72d7697b5a2784998b84a8
Ga eun Lee <hmyo2853@gmail.com>
2026-08-28 21:05:32 +0900

    fix(eslint-markdown): improve empty allow message for image URLs

--- packages/eslint-markdown/src/rules/allow-image-url.ts
+++ packages/eslint-markdown/src/rules/allow-image-url.ts
@@ -50,7 +50,7 @@ type RuleOptions = [
     allowDefinitions: string[];
   },
 ];
-type MessageIds = 'allowImageUrl' | 'disallowImageUrl';
+type MessageIds = 'allowImageUrl' | 'disallowImageUrl' | 'emptyAllowImageUrl';
 
 // --------------------------------------------------------------------------------
 // Rule Definition
@@ -116,6 +116,8 @@ export default {
         'The URL `{{ url }}` is not in the list of allowed URLs. (Allow: {{ patterns }}).',
       disallowImageUrl:
         'The URL `{{ url }}` is in the list of disallowed URLs. (Disallow: {{ patterns }}).',
+      emptyAllowImageUrl:
+        'The URL `{{ url }}` is not allowed because the list of allowed URLs is empty.',
     },
 
     language: 'markdown',
@@ -203,7 +205,15 @@ export default {
          */
 
         for (const { loc, url } of images) {
-          if (!allowUrls.some(regex => testRegexStateless(regex, url))) {
+          if (allowUrls.length === 0) {
+            context.report({
+              loc,
+              messageId: 'emptyAllowImageUrl',
+              data: {
+                url,
+              },
+            });
+          } else if (!allowUrls.some(regex => testRegexStateless(regex, url))) {
             context.report({
               loc,
               messageId: 'allowImageUrl',
